d4766023968dad3f2bbe62583d58f0156b400a2a
Vitaliy Galchinskiy <vitaliy.galchinskiy@jointoit.com>
2021-02-12 15:38:15 +0200

    cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const favicon = require('express-favicon');
+const cors = require('cors')
 const path = require('path');
 const port = process.env.PORT || 8080;
 
@@ -10,25 +11,7 @@ app.use(favicon(__dirname + '/build/favicon.png'));
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://chatback212.herokuapp.com');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors())
 
 //простой тест сервера
 app.get('/ping', function (req, res) {
