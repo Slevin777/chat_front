2e8202bfb1375e7d0a76924902fd391883cf64cf
Vitaliy Galchinskiy <vitaliy.galchinskiy@jointoit.com>
2021-02-12 15:00:55 +0200

    server

--- src/services/webSockets.js
+++ src/services/webSockets.js
@@ -1,3 +1,3 @@
 import socketIOClient from 'socket.io-client';
 
-export default socketIOClient(process.env.REACT_APP_API_URL);
+export default socketIOClient(process.env.REACT_APP_API_URL, { secure: true, reconnect: true, rejectUnauthorized: false });
