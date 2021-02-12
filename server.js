c05a48d656b2ae6c240bc10d9d21af72f2e2ba59
Vitaliy Galchinskiy <vitaliy.galchinskiy@jointoit.com>
2021-02-12 15:26:18 +0200

    add cors

--- server.js
+++ server.js
@@ -11,6 +11,25 @@ app.use(favicon(__dirname + '/build/favicon.png'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://chatback212.herokuapp.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 //простой тест сервера
 app.get('/ping', function (req, res) {
   return res.send('pong');
